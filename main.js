0e98b3d1a82693d0d9ff12ba3d0fadc47a5a39f4
Alec Reimel <alecreimel1@gmail.com>
2021-08-04 11:00:19 -0400

    init

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ HOLY MOUNTAIN COMPONENT - FOUC [Flash Of Unstyled Content]
 
 ********************************************/
 $(window).on("load", function() {
-	$('body').css('transition', '1.0s ease-in-out');
+	$('body').css('transition', '0.15s ease-in-out');
  	$('body').css('opacity', '1');
 });
